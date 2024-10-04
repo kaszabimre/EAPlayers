c55a5258a00b404a766b5caba7a51d7dfd412a06
Imre Kaszab <kaszab.imre@gmail.com>
2024-10-04 20:19:45 +0200

    fix: fix swiftlint issues and PR.yml

--- iosApp/iosApp/EAPlayersApp.swift
+++ iosApp/iosApp/EAPlayersApp.swift
@@ -2,13 +2,13 @@ import ComposeApp
 import SwiftUI
 
 @main
-struct iOSApp: App {
+struct EAPlayersApp: App {
     // Lazy so it doesn't try to initialize before startKoin() is called
-    lazy var logger = KoinApplication.getLogger(class: iOSApp.self)
-    
+    lazy var logger = KoinApplication.getLogger(class: EAPlayersApp.self)
+
     init() {
         KoinApplication.start()
-        logger.v(throwable: nil, tag: "Logger",message: { "App Started" })
+        logger.v(throwable: nil, tag: "Logger", message: { "App Started" })
     }
 
     var body: some Scene {
