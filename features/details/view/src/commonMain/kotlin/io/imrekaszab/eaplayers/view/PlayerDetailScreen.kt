4d621af16da1a56153f347a2b23b8007be7602be
Imre Kaszab <kaszab.imre@gmail.com>
2024-10-09 19:18:16 +0200

    feat: add LoadingIndicator

--- features/details/view/src/commonMain/kotlin/io/imrekaszab/eaplayers/view/PlayerDetailScreen.kt
+++ features/details/view/src/commonMain/kotlin/io/imrekaszab/eaplayers/view/PlayerDetailScreen.kt
@@ -1,12 +1,9 @@
 package io.imrekaszab.eaplayers.view
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -15,13 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import io.imrekaszab.eaplayers.core.util.invoke
 import io.imrekaszab.eaplayers.core.viewmodel.koinViewModel
 import io.imrekaszab.eaplayers.theme.AppTheme
 import io.imrekaszab.eaplayers.theme.navigation.EAPlayersScreens
+import io.imrekaszab.eaplayers.theme.widgets.LoadingIndicator
 import io.imrekaszab.eaplayers.ui.viewmodel.PlayerDetailViewModel
 
 @Composable
@@ -56,16 +53,7 @@ fun PlayerDetailScreen(
         contentColor = AppTheme.colorScheme.onPrimary
     ) { innerPadding ->
         when {
-            uiState.loading -> Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(AppTheme.colorScheme.background)
-            ) {
-                CircularProgressIndicator(
-                    modifier = Modifier.align(Alignment.Center),
-                    color = AppTheme.colorScheme.onPrimary
-                )
-            }
+            uiState.loading -> LoadingIndicator(modifier = Modifier.fillMaxSize())
 
             uiState.error != null ->
                 Text(text = "Error: ${uiState.error}")
